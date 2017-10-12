9fa34a91516fcb8dfeb88edbaa0f61eb36cb73e4
jjurman <jjurman@constantcontact.com>
2017-10-12 03:01:27 -0400

    add default route

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 const Tram = require('tram-one')
 
-const app = new Tram()
+const app = new Tram({defaultRoute: '/'})
 app.addRoute('/', require('./pages/home'))
 app.addActions({issuesStore: require('./actions/issues')})
 app.start('.main')
